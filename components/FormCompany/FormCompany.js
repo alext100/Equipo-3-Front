092cf5dd40546a478e2d054ba0842cefb44f4d07
Veronica Ortega <v.ortegaferreres@gmail.com>
2022-02-28 17:27:03 +0100

    prueba

---
+++ components/FormCompany/FormCompany.js
@@ -0,0 +1,12 @@
+const FormCompany = () => {
+  return (
+    <form>
+      <div>
+        <label>label</label>
+        <input type="text" />
+      </div>
+    </form>
+  );
+};
+
+export default FormCompany;
---
+++ pages/company-form.js
@@ -0,0 +1,5 @@
+const CompanyForm = () => {
+  return <h1>hola</h1>;
+};
+
+export default CompanyForm;
