d3dc4a106055e44c4293721b9598608d1259785e
Veronica Ortega <v.ortegaferreres@gmail.com>
2022-03-01 11:47:08 +0100

    cambios en el form

--- components/FormCompany/FormCompany.js
+++ components/FormCompany/FormCompany.js
@@ -1,31 +1,97 @@
 import { Layout } from "../Layout/Layout";
-import { Title,ButtonSave,Label,WrapperForm, Form, Camp, Input } from "./style";
+import { useState } from "react";
+import Router from "next/router";
+import {
+  Title,
+  ButtonSave,
+  Label,
+  WrapperForm,
+  Form,
+  Camp,
+  Input,
+} from "./style";
 
 const FormCompany = () => {
+  const [error, setError] = useState(false);
+  const [company, setCompany] = useState({
+    Name: "",
+    Website: "",
+    TotalFlights: "",
+    TotalSeats: "",
+  });
+
+  const { Name, Website, TotalFlights, TotalSeats } = company;
+  const handleChange = (e) => {
+    setCompany({
+      ...company,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    //validacion
+    if (
+      Name === "" ||
+      Website === "" ||
+      TotalFlights === "" ||
+      TotalSeats === ""
+    ) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    Router.push("/allcompanies");
+  };
+
   return (
     <Layout>
       <WrapperForm>
-    <Form column>
-      <Title>Add Company</Title>
-      <Camp>
-        <Label>label</Label>
-        <Input type="text" />
-      </Camp>
-      <Camp>
-        <Label>label</Label>
-        <Input type="text" />
-      </Camp>
-      <Camp>
-        <Label>label</Label>
-        <Input type="text" />
-      </Camp>
-      <Camp>
-        <Label>label</Label>
-        <Input type="text" />
-      </Camp>
-      <ButtonSave variant='contained'>Save Company</ButtonSave>
-    </Form>
-    </WrapperForm>
+        <Form onSubmit={onSubmitForm}>
+          <Title>Add Company</Title>
+          <Camp>
+            <Label>Name</Label>
+            <Input
+              type="text"
+              name="Name"
+              value={Name}
+              onChange={handleChange}
+            />
+          </Camp>
+          <Camp>
+            <Label>Website</Label>
+            <Input
+              type="text"
+              name="Website"
+              value={Website}
+              onChange={handleChange}
+            />
+          </Camp>
+          <Camp>
+            <Label>Total flights</Label>
+            <Input
+              type="text"
+              name="TotalFlights"
+              value={TotalFlights}
+              onChange={handleChange}
+            />
+          </Camp>
+          <Camp>
+            <Label>Total seats</Label>
+            <Input
+              type="text"
+              name="TotalSeats"
+              value={TotalSeats}
+              onChange={handleChange}
+            />
+          </Camp>
+          <ButtonSave type="submit" variant="contained">
+            Save Company
+          </ButtonSave>
+          {error ? <p> Rellena todos los campos</p> : null}
+        </Form>
+      </WrapperForm>
     </Layout>
   );
 };
